189f0f8bc692dc0d41e9cfe612b7bebdfb64c6c2
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-09-04 15:26:11 -0300

    Add: clase factura

--- src/org/marvic/appFacturas/models/Factura.java
+++ src/org/marvic/appFacturas/models/Factura.java
@@ -1,4 +1,38 @@
 package org.marvic.appFacturas.models;
 
+import java.util.Date;
+
 public class Factura {
+    private long id;
+    private String description;
+    private Date date;
+
+    public Factura(Date date) {
+        this.date = date;
+    }
+
+    public Factura( String description, Date date) {
+        this.description = description;
+        this.date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
