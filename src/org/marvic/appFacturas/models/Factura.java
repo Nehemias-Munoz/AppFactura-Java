400a9a3916ff2bcd093450bb1566706b56246fdf
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-09-04 16:50:05 -0300

    Add: metodos calcularTotal, obtener detalles facturas

--- src/org/marvic/appFacturas/models/Factura.java
+++ src/org/marvic/appFacturas/models/Factura.java
@@ -1,5 +1,7 @@
 package org.marvic.appFacturas.models;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -8,7 +10,7 @@ public class Factura {
     private static long lastId;
     private String description;
     private Date date;
-    private Cliente cliente;
+    private Customer customer;
     private ArrayList<ItemsFactura> item;
 
     public Factura() {
@@ -41,19 +43,73 @@ public class Factura {
         this.date = date;
     }
 
-    public Cliente getCliente() {
-        return cliente;
+    public Customer getCustomer() {
+        return customer;
     }
 
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 
     public ArrayList<ItemsFactura> getItems() {
         return item;
     }
 
+    /***
+     * AddItemFactura
+     * añade un elemtento de tipo ItemsFactura a la lista
+     * @param item
+     */
     public void addItemFactura(ItemsFactura item){
          this.item.add(item);
+    }
+
+    /***
+     * calculateTotal
+     * Recorre la lista con productos sumando su valor
+     * @return float valor total de la factura
+     */
+    public float calculateTotal(){
+        float total = 0.0f;
+        for (ItemsFactura item: getItems()) {
+            total += item.calculateAmount();
+        }
+        return total;
+    }
+
+    /***
+     * getDetails
+     * Parrafo con formato e informacion de la factura
+     * @return String Detalles de la factura
+     */
+    public String getDetails(){
+        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
+        StringBuilder detail = new StringBuilder("Factura N:");
+        detail.append(id);
+        detail.append("\nFecha: ")
+                        .append(dateFormat.format(date));
+        detail.append("\nCliente: ")
+                .append(this.getCustomer().getName());
+        detail.append("\t Rut: ")
+                .append(customer.getRut());
+        detail.append("\nDescripcion: ")
+                .append(this.description)
+                .append("\n");
+        detail.append("\n #\t Nombre\tValor\tCantidad\tTotal\n");
+        for (ItemsFactura item:this.getItems()) {
+            detail.append(item.getProduct().getId())
+                    .append("\t")
+                    .append(item.getProduct().getName())
+                    .append("\t")
+                    .append(item.getProduct().getPrice())
+                    .append("\t")
+                    .append(item.getQuantity())
+                    .append("\t")
+                    .append(item.calculateAmount())
+                    .append("\n");
+        }
+        detail.append("\nValor Total:")
+                .append(calculateTotal());
+        return detail.toString();
     }
 }
