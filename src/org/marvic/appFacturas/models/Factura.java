50b99665a2177df2c67bbd0fa88237cb823dc951
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-09-04 15:47:11 -0300

    Add: relacion con producto e items, Upd: generacion de id

--- src/org/marvic/appFacturas/models/Factura.java
+++ src/org/marvic/appFacturas/models/Factura.java
@@ -1,17 +1,22 @@
 package org.marvic.appFacturas.models;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Factura {
     private long id;
+    private static long lastId;
     private String description;
     private Date date;
+    private Cliente cliente;
+    private ArrayList<ItemsFactura> item;
 
-    public Factura(Date date) {
-        this.date = date;
+    public Factura() {
+        this.id = ++lastId;
     }
 
     public Factura( String description, Date date) {
+        this();
         this.description = description;
         this.date = date;
     }
@@ -34,5 +39,21 @@ public class Factura {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ArrayList<ItemsFactura> getItems() {
+        return item;
+    }
+
+    public void addItemFactura(ItemsFactura item){
+         this.item.add(item);
     }
 }
