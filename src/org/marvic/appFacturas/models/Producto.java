d7c76825e0123185bdf9da65102a44e8983701e4
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-09-04 14:44:56 -0300

    Add: clase producto

--- src/org/marvic/appFacturas/models/Producto.java
+++ src/org/marvic/appFacturas/models/Producto.java
@@ -1,4 +1,39 @@
 package org.marvic.appFacturas.models;
 
+/**
+ * @author nehemiasmunoz
+ */
 public class Producto {
+    private int id;
+    private String name;
+    private float price;
+
+    public Producto() {
+    }
+
+    public Producto(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
