f87d6b63b75bd203f8faecae800d885526a44853
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-09-04 15:25:59 -0300

    Add: clase cliente

--- src/org/marvic/appFacturas/models/Cliente.java
+++ src/org/marvic/appFacturas/models/Cliente.java
@@ -1,4 +1,33 @@
 package org.marvic.appFacturas.models;
 
+/***
+ * @author nehemiasmunoz
+ */
 public class Cliente {
+    private String name;
+    private int rut;
+
+    public Cliente() {
+    }
+
+    public Cliente(String name, int rut) {
+        this.name = name;
+        this.rut = rut;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
 }
